6390105fad1d99818f92dd9e9a86ad7e1a62c035
Chiara Coletti <c.coletti@tudelft.nl>
2023-03-22 12:31:27 +0100

    Added new feature to optionally display textual summary of statistics

--- inflammation-analysis.py
+++ inflammation-analysis.py
@@ -17,13 +17,18 @@ def main(args):
     if not isinstance(InFiles, list):
         InFiles = [args.infiles]
 
-
     for filename in InFiles:
         inflammation_data = models.load_csv(filename)
 
-        view_data = {'average': models.daily_mean(inflammation_data), 'max': models.daily_max(inflammation_data), 'min': models.daily_min(inflammation_data)}
+        view_data = {'average': models.daily_mean(inflammation_data), 'max': models.daily_max(inflammation_data),
+                     'min': models.daily_min(inflammation_data)}
+
+        #print(type(args.printText))
+        if args.printText:
+            views.textual_view(view_data)
 
         views.visualize(view_data)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -33,6 +38,13 @@ if __name__ == "__main__":
         'infiles',
         nargs='+',
         help='Input CSV(s) containing inflammation series for each patient')
+
+    parser.add_argument(
+        '--printText',
+        required=False,
+        default=False,
+        type=bool,
+        help='Option to display text summary of inflammation data statistics')
 
     args = parser.parse_args()
 
